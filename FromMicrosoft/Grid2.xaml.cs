622af590437153ecf3f91a8c261ff5a9b7d9bd82
sabinusi <sabinusimlambo@gmail.com>
2019-12-15 20:02:38 +0300

    adding gerid 2

---
+++ FromMicrosoft/Grid2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FromMicrosoft
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Grid2 : ContentView
+    {
+        public Grid2()
+        {
+            InitializeComponent();
+        }
+    }
+}
